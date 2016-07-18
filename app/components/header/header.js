eb9c85c55810d07193bceef4bb9c0617d1ca033e
Elaine Huang <yi-ling.huang@hopebaytech.com>
2016-07-18 23:23:02 +0800

    add header css

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import './header.scss';
 import '../style/general.scss';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="layout-row vertical-top align-center castle-background">
-        <Navbar inverse className="header">
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Hopebay Flea Market</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">Log out</NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <div className="layout-row vertical-top castle-background">
+        <ul className="layout-row align-between">
+          <li><a href="">Flea Market</a></li>
+          <li><a href="">Logout</a></li>
+        </ul>
       </div>
     );
   }
