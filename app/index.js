d27fadc04fb308e7261fea70b1e0cb16543f2bef
ben196888 <ben196888@gmail.com>
2016-08-01 18:49:05 +0800

    Add simple react-router in index, move App.js from containers to components

--- app/index.js
+++ app/index.js
@@ -2,6 +2,13 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import {
+  Router,
+  Route,
+  // IndexRoute,
+  browserHistory,
+} from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import configureStore from './stores/configureStore';
 import App from './components/App';
@@ -9,11 +16,14 @@ import DevTools from './containers/DevTools';
 import './index.scss';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
     <div>
-      <App />
+      <Router history={history}>
+        <Route path="/" component={App} />
+      </Router>
       <DevTools />
     </div>
   </Provider>,
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 
 import goods from './goods';
 
 const fleaMarketApp = combineReducers({
   goods,
+  routing: routerReducer,
 });
 
 export default fleaMarketApp;
