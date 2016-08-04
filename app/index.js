f95fddeb91f5917b4539c4ae32295efb87a177ba
ben196888 <ben196888@gmail.com>
2016-08-04 20:20:30 +0800

    Add simple react-router example

--- app/index.js
+++ app/index.js
@@ -5,13 +5,15 @@ import { Provider } from 'react-redux';
 import {
   Router,
   Route,
-  // IndexRoute,
+  IndexRoute,
   browserHistory,
 } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import configureStore from './stores/configureStore';
 import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
 import DevTools from './containers/DevTools';
 import './index.scss';
 
@@ -22,7 +24,10 @@ render(
   <Provider store={store}>
     <div>
       <Router history={history}>
-        <Route path="/" component={App} />
+        <Route path="/" component={App}>
+          <IndexRoute component={Home} />
+          <Route path="about" component={About} />
+        </Route>
       </Router>
       <DevTools />
     </div>
