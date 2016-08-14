99a1fd60c9b66949883e0b6b5bf70bb92c24ebd2
Elaine Huang <yi-ling.huang@hopebaytech.com>
2016-08-14 21:25:33 +0800

    add bella

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header/header';
-
+import belle from 'belle';
+const Button = belle.Button;
 // import { connect } from 'react-redux';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Button primary>Follow</Button>
       Hello World! Ya
     </div>
   );
