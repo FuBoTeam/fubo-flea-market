b8827deee663e6f2a8c4c88057166142bcc675f3
ben196888 <ben196888@gmail.com>
2016-10-03 14:48:30 +0800

    Init graphql middleware

--- app/stores/configureStore.dev.js
+++ app/stores/configureStore.dev.js
@@ -6,7 +6,9 @@ import {
 import { routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import { persistState } from 'redux-devtools';
+import graphqlMiddleware from 'redux-graphql-middleware';
 import thunk from 'redux-thunk';
+import { fetch } from 'redux-auth';
 // import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from '../reducers';
@@ -20,10 +22,22 @@ function getDebugSessionKey() {
   return (matches && matches.length > 0) ? matches[1] : null;
 }
 
+const graphOptions = {
+  fetch,
+  server: 'http://flea.fubotech.com.tw/graphql',
+  action: 'GRAPH',
+  ready: 'GRAPH_READY',
+  done: 'GRAPH_DONE',
+  error: 'GRAPH_ERROR',
+  transform: (data) => { return data; },
+  errorTransform: (error) => { return error; },
+};
+
 const enhancer = compose(
   // applyMiddleware(thunk, saga),
   applyMiddleware(
     routerMiddleware(browserHistory),
+    graphqlMiddleware(graphOptions),
     thunk
   ),
   window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
--- app/stores/configureStore.prod.js
+++ app/stores/configureStore.prod.js
@@ -5,7 +5,9 @@ import {
 } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router';
+import graphqlMiddleware from 'redux-graphql-middleware';
 import thunk from 'redux-thunk';
+import { fetch } from 'redux-auth';
 // import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from '../reducers';
@@ -13,8 +15,22 @@ import rootReducer from '../reducers';
 
 // const saga = createSagaMiddleware()
 
+const graphOptions = {
+  fetch,
+  server: 'http://flea.fubotech.com.tw/graphql',
+  action: 'GRAPH',
+  ready: 'GRAPH_READY',
+  done: 'GRAPH_DONE',
+  error: 'GRAPH_ERROR',
+  transform: (data) => { return data; },
+  errorTransform: (error) => { return error; },
+};
+
 // const enhancer = applyMiddleware(thunk, saga)
-const enhancer = applyMiddleware(routerMiddleware(browserHistory), thunk);
+const enhancer = applyMiddleware(
+  routerMiddleware(browserHistory),
+  graphqlMiddleware(graphOptions),
+  thunk);
 
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
