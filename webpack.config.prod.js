95084e0a3c250a043e28be2868d975ae3d0d4c9d
ben196888 <ben196888@gmail.com>
2016-10-19 11:11:16 +0800

    Split webpack config. Deploy to github page.

---
+++ webpack.config.prod.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const postcssNested = require('postcss-nested');
+const postcssNext = require('postcss-cssnext');
+
+module.exports = {
+  entry: {
+    javascript: [
+      path.resolve(__dirname, 'app/index.js'),
+    ],
+    html: path.resolve(__dirname, 'app/index.html'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  postcss: [
+    postcssNested,
+    postcssNext,
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'eslint',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'app'),
+        exclude: /node_modules/,
+      },
+      {
+      test: /\.[s]?css$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'postcss',
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|woff)$/,
+        loader: 'url-loader?limit=8192?name=[name].[ext]',
+      },
+      {
+        test: /\.woff?$/,
+        loader: 'url?limit=100000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file',
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
+  eslint: {
+    configFile: './.eslintrc',
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+  ],
+};
